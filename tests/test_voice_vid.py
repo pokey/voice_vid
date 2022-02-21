5c5826e53c0959739f6e1794bd99bb08e566c4e5
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-02-21 14:25:01 +0000

    Cleanup

--- tests/test_voice_vid.py
+++ tests/test_voice_vid.py
@@ -9,6 +9,8 @@ runner = CliRunner()
 def test_app():
     raw_transcript = str(get_data_file("raw-transcript.jsonl"))
     expected_output = get_data_file("expected-output.srt").read_text()
+
     result = runner.invoke(app, [raw_transcript])
+
     assert result.exit_code == 0
     assert result.stdout == expected_output

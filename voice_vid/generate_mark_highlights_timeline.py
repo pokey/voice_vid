d3af2f8a6adc48fbbd5f0b2b382072ce68628def
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-03-07 16:43:56 +0000

    Extracts some functions

--- voice_vid/generate_mark_highlights_timeline.py
+++ voice_vid/generate_mark_highlights_timeline.py
@@ -42,11 +42,6 @@ def generate_mark_highlights_timeline(
     media_reference = clip.media_reference
     framerate = int(media_reference.metadata["fcp_xml"]["rate"]["timebase"])
 
-    track = otio.schema.Track(
-        name="",
-        metadata={"fcp_xml": {"enabled": "TRUE", "locked": "FALSE"}},
-    )
-
     items = [
         otio.schema.Gap(
             name="",
@@ -64,86 +59,77 @@ def generate_mark_highlights_timeline(
             ),
         ),
         create_transition(),
-        otio.schema.Clip(
-            name="Screen Recording 2022-03-01 at 17.34.23.mov",
-            media_reference=media_reference,
-            source_range=otio.opentime.TimeRange(
-                start_time=otio.opentime.RationalTime(value=755411, rate=60),
-                duration=otio.opentime.RationalTime(value=220, rate=60),
-            ),
-            metadata={
-                "fcp_xml": {
-                    "filter": [
-                        {
-                            "effect": {
-                                "effectcategory": "motion",
-                                "effectid": "timeremap",
-                                "effecttype": "motion",
-                                "mediatype": "video",
-                                "name": "Time Remap",
-                                "parameter": [
-                                    {
-                                        "name": "speed",
-                                        "parameterid": "speed",
-                                        "value": "0.997122",
-                                        "valuemax": "10000",
-                                        "valuemin": "-10000",
-                                    },
-                                ],
-                            },
-                            "enabled": "TRUE",
-                            "end": "-1",
-                            "start": "-1",
-                        },
-                    ],
-                }
-            },
+        create_freeze_frame(
+            media_reference,
+            time=otio.opentime.RationalTime(value=7532.36927142, rate=60),
+            duration=otio.opentime.RationalTime(value=220, rate=60),
         ),
         create_transition(),
-        otio.schema.Clip(
-            name="Screen Recording 2022-03-01 at 17.34.23.mov",
-            media_reference=media_reference,
-            source_range=otio.opentime.TimeRange(
-                start_time=otio.opentime.RationalTime(value=753003, rate=60),
-                duration=otio.opentime.RationalTime(value=10, rate=60),
-            ),
-            metadata={
-                "fcp_xml": {
-                    "filter": [
-                        {
-                            "effect": {
-                                "effectcategory": "motion",
-                                "effectid": "timeremap",
-                                "effecttype": "motion",
-                                "mediatype": "video",
-                                "name": "Time Remap",
-                                "parameter": [
-                                    {
-                                        "name": "speed",
-                                        "parameterid": "speed",
-                                        "value": "0.997122",
-                                        "valuemax": "10000",
-                                        "valuemin": "-10000",
-                                    },
-                                ],
-                            },
-                            "enabled": "TRUE",
-                            "end": "-1",
-                            "start": "-1",
-                        },
-                    ],
-                }
-            },
+        create_freeze_frame(
+            media_reference,
+            time=otio.opentime.RationalTime(value=7508.35857366, rate=60),
+            duration=otio.opentime.RationalTime(value=10, rate=60),
         ),
     ]
 
-    for item in items:
-        track.append(item)
+    track = otio.schema.Track(
+        name="",
+        metadata={"fcp_xml": {"enabled": "TRUE", "locked": "FALSE"}},
+        children=items,
+    )
 
     timeline = copy.deepcopy(input_timeline)
     timeline.tracks[:] = [track]
 
     return timeline
+
+
+FREEZE_FRAME_SPEED = 0.997122 / 100
+FREEZE_FRAME_SPEED_STR = str(FREEZE_FRAME_SPEED * 100)
+
+
+def create_freeze_frame(
+    media_reference: otio.schema.ExternalReference,
+    time: otio.opentime.RationalTime,
+    duration: otio.opentime.RationalTime,
+):
+    start_time = otio.opentime.RationalTime(time.value / FREEZE_FRAME_SPEED, time.rate)
+
+    return otio.schema.Clip(
+        name="Screen Recording 2022-03-01 at 17.34.23.mov",
+        media_reference=media_reference,
+        source_range=otio.opentime.TimeRange(
+            start_time=start_time,
+            duration=duration,
+        ),
+        metadata={
+            "fcp_xml": {
+                "filter": [
+                    {
+                        "effect": {
+                            "effectcategory": "motion",
+                            "effectid": "timeremap",
+                            "effecttype": "motion",
+                            "mediatype": "video",
+                            "name": "Time Remap",
+                            "parameter": [
+                                {
+                                    "name": "speed",
+                                    "parameterid": "speed",
+                                    "value": FREEZE_FRAME_SPEED_STR,
+                                    "valuemax": "10000",
+                                    "valuemin": "-10000",
+                                },
+                            ],
+                        },
+                        "enabled": "TRUE",
+                        "end": "-1",
+                        "start": "-1",
+                    },
+                ],
+            }
+        },
+    )
 
 
 def create_transition():
